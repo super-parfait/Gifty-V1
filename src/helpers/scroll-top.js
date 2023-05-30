62a03639c6f25f1e243f9744e192454f94828dae
super-parfait <djiri.kouame@uvci.edu.ci>
2023-05-30 12:32:48 +0000

    Mise a jour des packages et Inscription ok, connexion oK

--- src/helpers/scroll-top.js
+++ src/helpers/scroll-top.js
@@ -1,11 +1,43 @@
 import { useEffect } from "react";
 import { withRouter } from "react-router-dom";
+// import {
+//   useLocation,
+//   useNavigate,
+//   useParams,
+// } from "react-router-dom";
+
+// function withRouter(Component) {
+//   function ComponentWithRouterProp(props) {
+//     let location = useLocation();
+//     let navigate = useNavigate();
+//     let params = useParams();
+//     return (
+//       <Component
+//         {...props}
+//         router={{ location, navigate, params }}
+//       />
+//     );
+//   }
+
+//   return ComponentWithRouterProp;
+// }
+
+// import { useNavigate } from 'react-router';
+
+
+// function withRouter(Component){
+//     const Wrapper = (props) =>{
+//         const history = useNavigate();
+//         return <Component history={history} {...props}/> 
+//     } 
+//     return Wrapper;
+// }
 
 const ScrollToTop = props => {
   useEffect(() => {
     window.scrollTo(0, 0);
   });
-  return props.children;
+  return props.children;  
 };
 
 export default withRouter(ScrollToTop);
